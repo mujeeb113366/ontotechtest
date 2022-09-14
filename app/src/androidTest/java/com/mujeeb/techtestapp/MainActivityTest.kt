bc5d0d85d9e89044efaaaf835533c6ff7e21eea6
Mujeeb Adisa <mujeeb@tympahealth.com>
2022-09-14 21:02:16 +0100

    UI tests for the refresh feature to test whether the values displayed are as expected

---
+++ app/src/androidTest/java/com/mujeeb/techtestapp/MainActivityTest.kt
@@ -0,0 +1,33 @@
+package com.mujeeb.techtestapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mujeeb.techtestapp.presentation.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
+
+
+    @Test
+    fun should_pass_when_ExpectedMileageValue() {
+        onView(withId(R.id.fb_refresh_user_data)).perform(click())
+        onView(withId(R.id.tv_mileage_level)).check(matches(withText("747")))
+    }
+
+
+    @Test
+    fun should_pass_when_ExpectedEnergyLevel() {
+        onView(withId(R.id.fb_refresh_user_data)).perform(click())
+        onView(withId(R.id.tv_energy_level)).check(matches(withText("72%")))
+    }
+}
