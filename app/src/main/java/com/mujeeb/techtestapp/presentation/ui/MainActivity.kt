b2962ee35f0c3c502f30b720c6c3cefe66783610
Mujeeb Adisa <mujeeb@tympahealth.com>
2022-09-14 17:08:49 +0100

    Add refresh button

--- app/src/main/java/com/mujeeb/techtestapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/mujeeb/techtestapp/presentation/ui/MainActivity.kt
@@ -40,7 +40,9 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-
+        binding.fbRefreshUserData.setOnClickListener {
+            mainViewModel.getUserData()
+        }
         mainViewModel.getUserData()
 
     }
